7c1538de0474dca3262fe2fedd58dbdb03d5cf63
Yahsai <yahsaisantana1@gmail.com>
2024-01-24 15:17:17 -0400

    Task 5

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = world [:3]
+word_last_2 = world [:-2]
+middle word = world [1: -1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
