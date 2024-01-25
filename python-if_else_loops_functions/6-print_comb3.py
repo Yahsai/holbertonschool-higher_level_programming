56c6358fc6132c061264788a7e49045805bf90de
Yahsai <yahsaisantana1@gmail.com>
2024-01-25 14:01:10 -0400

    Task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num in range(0, 10):
+    for pos in range(0, 10):
+        if num !>= pos:
+            pass
+        elif num == 8 and pos == 9:
+            print("{}{}".format(num, pos))
+        else:
+            print("{}}{}".format(num, pos), end=", ")
