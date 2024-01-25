9ce1caadc10f547cee265d213d341e5f60538fc0
Yahsai <yahsaisantana1@gmail.com>
2024-01-25 12:44:04 -0400

    Task 2 alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    if chr(letter) not in ['e', 'q']:
+for i in range(97, 123): 
+    if i !=101 and i != 113:
         print("{}".format(chr(letter)), end='')
-        
