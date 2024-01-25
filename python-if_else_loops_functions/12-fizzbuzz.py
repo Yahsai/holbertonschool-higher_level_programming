c77aa01c8af585f4f1885e6636bc7ac9aae4d833
Yahsai <yahsaisantana1@gmail.com>
2024-01-25 14:57:04 -0400

    Task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+ef fizzbuzz():
+    for num in range(1, 101):
+        # Verifica si es múltiplo de ambos 3 y 5 primero
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz", end=" ")
+        # Luego, verifica si es múltiplo de 3
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        # Después, verifica si es múltiplo de 5
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        # Si no es múltiplo ni de 3 ni de 5, simplemente imprime el número
+        else:
+            print(num, end=" ")
