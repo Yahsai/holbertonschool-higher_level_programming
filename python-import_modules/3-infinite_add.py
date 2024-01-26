f0b004cdf3b1aea2f96bead39fa510d90f388cc0
Yahsai <yahsaisantana1@gmail.com>
2024-01-25 23:37:37 -0400

    Update

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
 
-ac = len(sys.argv)
-if ac == 1:
-    print("0")
-else:
-    sum = 0
-    for j in range(1, ac):
-        sum += int(sys.argv[j])
-    print("{}".format(sum))
+import sys
+
+if __name__ == "__main__":
+
+    args = sys.argv[1:]
+
+    num = []
+    for i in args:
+        num.append(int(i))
+
+    total = sum(num)
+    print(total, end="\n")
