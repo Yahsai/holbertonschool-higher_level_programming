ea0cc18fc9896389fb2dfe7d8f0d485fd065fd72
Yahsai <yahsaisantana1@gmail.com>
2024-01-31 22:34:31 -0400

    Task 3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -11,5 +11,8 @@ def common_elements(set_1, set_2):
     - A set containing common elements from set_1 and set_2.
     """
     # Use the intersection operation to find common elements
-    common_set = set_1 & set_2
+    common_set = set()
+    for element in set_1:
+        if element in set_2:
+            common_set.add(element)
     return common_set
