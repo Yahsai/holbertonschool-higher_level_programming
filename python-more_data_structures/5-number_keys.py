5e768af59e71bc5141df35a75b0a1f9d8351c7f8
Yahsai <yahsaisantana1@gmail.com>
2024-01-31 22:37:49 -0400

    Task 5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    Args:
+    - a_dictionary: The input dictionary.
+
+    Returns:
+    - The number of keys in the dictionary.
+    """
+    # Use the len() function to determine the number of keys
+    return len(a_dictionary)
