cb7de06681e2d869afa9edb40a3c2a11cd046e93
Yahsai <yahsaisantana1@gmail.com>
2024-01-31 22:13:59 -0400

    Task 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,24 @@
+def search_replace(my_list, search, replace):
+    """
+    Replace all occurrences of 'search' with 'replace' in a new list.
+
+    Args:
+    - my_list: The initial list.
+    - search: The element to replace in the list.
+    - replace: The new element.
+
+    Returns:
+    - A new list with all occurrences of 'search' replaced by 'replace'.
+    """
+    # Create a new list to store the modified elements
+    new_list = []
+
+    # Iterate through the elements of the original list
+    for item in my_list:
+        # Replace 'search' with 'replace' if found, otherwise keep the original element
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+
+    return new_list
