6aee93a53ad85bc4a9d8c7226d30c0eb5088f27b
Yahsai <yahsaisantana1@gmail.com>
2024-02-02 00:56:26 -0400

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,39 @@
-#!/usr/bin/python3
-"""
-Zeroth exercise for doctest
-"""
+/bin/python3
+"""add_integer - function
+Attribute:
+    a: first integer
+    b: second integer
+return: sum of a and b"""
 
 
 def add_integer(a, b=98):
-    """
-    If a and b aren't floats or integers,
-    this function raises TypeError.
-    Otherwise, it returns a + b.
-    """
-    if type(a) != int and type(a) != float:
+    """Returns sum of a and b
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer("School", -30)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+    >>> add_integer()
+    Traceback (most recent call last):
+        ...
+    TypeError: add_integer() missing 1 required positional argument: 'a' """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-
     return int(a) + int(b)
