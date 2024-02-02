a3e55fce0e44a709325377fcee8afd5ce83fa17d
Yahsai <yahsaisantana1@gmail.com>
2024-02-01 22:43:38 -0400

    Task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Square module - provides an empty class Square
+"""
+
+
+class Square:
+    """This is a Square class.
+
+    This class doesn't have any attributes or methods yet, but it can be used
+    as a starting point for creating a Square object.
+
+    """
+    pass
