c8e9983a43b8aa7171f95494ec6f0f6c47cc4b3c
Yahsai <yahsaisantana1@gmail.com>
2024-02-09 02:14:51 -0400

    Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,59 +1,14 @@
 #!/usr/bin/python3
+"""8. Class to JSON"""
+
+
 def class_to_json(obj):
-    """
-    Convert an object to a JSON-serializable dictionary.
-
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
     Args:
-    - obj: An instance of a class with serializable attributes (list, dictionary, string, integer, and boolean).
-
-    Returns:
-    - A dictionary representing the serialized form of the object.
+        obj: object to be serialized
     """
-    if not hasattr(obj, '__dict__'):
-        raise TypeError("Input object must be an instance of a class")
+    return obj.__dict__
 
-    serializable_dict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            serializable_dict[key] = value
-        elif isinstance(value, MyClass):  # Handle nested objects of type MyClass
-            serializable_dict[key] = class_to_json(value)
-        elif value is None:
-            serializable_dict[key] = None
-        else:
-            raise TypeError(f"Attribute '{key}' has an unsupported type: {type(value).__name__}")
-
-    return serializable_dict
-
-class MyClass:
-    """
-    A class with serializable attributes for JSON conversion.
-    """
-    def __init__(self, name, number=0):
-        self.__name = name
-        self.number = number
-        self.is_team_red = (self.number % 2) == 0
-        self.score = 0
-
-    def win(self):
-        self.score += 1
-
-    def lose(self):
-        self.score -= 1
-
-    def __str__(self):
-        return "[MyClass] {} - {:d} => {:d}".format(self.__name, self.number, self.score)
-
-# Example usage
-if __name__ == "__main__":
-    m = MyClass("John")
-    m.win()
-
-    print(type(m))
-    print(m)
-
-    mj = class_to_json(m)
-    print(type(mj))
-    print(mj)
 
 
