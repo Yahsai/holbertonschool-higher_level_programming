f09a6ac744d5282e2875f71cbb772188b4cbaa26
Yahsai <yahsaisantana1@gmail.com>
2024-02-09 01:00:35 -0400

    Task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for Square class."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize a Square instance.
+        Args:
+            size (int): The size of the square."""
+        self.__size = size
+        super().integer_validator("size", size)
+
+    def area(self):
+        """Calculate the area of the square.
+        Returns:
+            int: The area of the square."""
+        return self.__size ** 2
