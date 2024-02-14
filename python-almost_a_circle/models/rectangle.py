459cdf7ae5d90f6fe330b600b88923074ad8c57b
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 19:46:52 -0400

    Update rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,8 +87,24 @@ class Rectangle(Base):
         return (f"[Rectangle] ({self.id}) "
                 f"{self.x}/{self.y} - {self.width}/{self.height}")
         
-    def update(self, *args):
-        """Assigns each argument to the corresponding attribute."""
-        attrs = ["id", "width", "height", "x", "y"]
-        for i, arg in enumerate(args):
-            setattr(self, attrs[i], arg)
+    def update(self, *args, **kwargs):
+        """Update the rectangle"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """return the dictionary representation of a Rectangle"""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
