5e7f23cdae23760ac3ac088d54891d490e2c2ddc
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 15:08:40 -0400

    fix base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+import turtle
+
 class Base:
     __nb_objects = 0
 
@@ -9,19 +12,70 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-if __name__ == "__main__":
-    b1 = Base()
-    print(b1.id)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-    b2 = Base()
-    print(b2.id)
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
 
-    b3 = Base()
-    print(b3.id)
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
 
-    b4 = Base(12)
-    print(b4.id)
+    @classmethod
+    def create(cls, **dictionary):
+        dummy_instance = cls(1, 1)  # Creating a dummy instance with arbitrary values
+        dummy_instance.update(**dictionary)
+        return dummy_instance
 
-    b5 = Base()
-    print(b5.id)
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+                list_dicts = cls.from_json_string(json_string)
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
 
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        turtle.speed(2)
+        for rect in list_rectangles:
+            turtle.penup()
+            turtle.goto(rect.x, rect.y)
+            turtle.pendown()
+            turtle.forward(rect.width)
+            turtle.left(90)
+            turtle.forward(rect.height)
+            turtle.left(90)
+            turtle.forward(rect.width)
+            turtle.left(90)
+            turtle.forward(rect.height)
+            turtle.left(90)
+
+        for square in list_squares:
+            turtle.penup()
+            turtle.goto(square.x, square.y)
+            turtle.pendown()
+            turtle.forward(square.size)
+            turtle.left(90)
+            turtle.forward(square.size)
+            turtle.left(90)
+            turtle.forward(square.size)
+            turtle.left(90)
+            turtle.forward(square.size)
+            turtle.left(90)
