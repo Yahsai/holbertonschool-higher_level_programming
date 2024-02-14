a3570474aa869131d1dd10305ab2bfb01c394de3
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 15:39:54 -0400

    14 main file

--- python-almost_a_circle/14-main.py
+++ python-almost_a_circle/14-main.py
@@ -4,6 +4,7 @@ from models.base import Base
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
+
     r1 = Rectangle(10, 7, 2, 8)
     dictionary = r1.to_dictionary()
     json_dictionary = Base.to_json_string([dictionary])
@@ -11,4 +12,3 @@ if __name__ == "__main__":
     print(type(dictionary))
     print(json_dictionary)
     print(type(json_dictionary))
-
