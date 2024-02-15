e2a8e42d127968cff15ac0561c14e0fb30b4ca68
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 23:21:42 -0400

    Update test square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,78 +1,32 @@
 import unittest
 from models.square import Square
 
+
 class TestSquare(unittest.TestCase):
-    def test_init(self):
-        s1 = Square(5)
-        self.assertEqual(s1.id, 1)
-        self.assertEqual(s1.size, 5)
-        self.assertEqual(s1.width, 5)
-        self.assertEqual(s1.height, 5)
-        self.assertEqual(s1.x, 0)
-        self.assertEqual(s1.y, 0)
-
-        s2 = Square(2, 2)
-        self.assertEqual(s2.id, 2)
-        self.assertEqual(s2.size, 2)
-        self.assertEqual(s2.width, 2)
-        self.assertEqual(s2.height, 2)
-        self.assertEqual(s2.x, 2)
-        self.assertEqual(s2.y, 0)
-
-        s3 = Square(3, 1, 3, 12)
-        self.assertEqual(s3.id, 12)
-        self.assertEqual(s3.size, 3)
-        self.assertEqual(s3.width, 3)
-        self.assertEqual(s3.height, 3)
-        self.assertEqual(s3.x, 1)
-        self.assertEqual(s3.y, 3)
-
-    def test_setters(self):
-        s = Square(1, 1, 1, 1)
-
-        with self.assertRaises(TypeError):
-            s.size = "hello"
-        with self.assertRaises(ValueError):
-            s.size = 0
-
-        with self.assertRaises(TypeError):
-            s.x = "hello"
-        with self.assertRaises(ValueError):
-            s.x = -1
-
-        with self.assertRaises(TypeError):
-            s.y = "hello"
-        with self.assertRaises(ValueError):
-            s.y = -1
-
-    def test_area(self):
-        s = Square(5)
-        self.assertEqual(s.area(), 25)
-
-    def test_display(self):
-        s = Square(3)
-        s.display()
-
     def test_str(self):
-        s = Square(4, 2, 1, 12)
-        self.assertEqual(str(s), "[Square] (12) 2/1 - 4")
+        s = Square(5, 3, 4, 1)
+        self.assertEqual(str(s), "[Square] (1) 3/4 - 5")
+
+    def test_size(self):
+        s = Square(10)
+        self.assertEqual(s.size, 10)
+        s.size = 20
+        self.assertEqual(s.size, 20)
+        self.assertEqual(s.width, 20)
+        self.assertEqual(s.height, 20)
 
     def test_update(self):
-        s = Square(10, 10, 10, 10)
-        s.update(89)
-        self.assertEqual(str(s), "[Square] (89) 10/10 - 10")
-        s.update(89, 2)
-        self.assertEqual(str(s), "[Square] (89) 10/10 - 2")
-        s.update(89, 2, 3)
-        self.assertEqual(str(s), "[Square] (89) 10/10 - 2")
-        s.update(89, 2, 3, 4)
-        self.assertEqual(str(s), "[Square] (89) 4/10 - 2")
-        s.update(89, 2, 3, 4, 5)
-        self.assertEqual(str(s), "[Square] (89) 4/5 - 2")
+        s = Square(5, 3, 4, 1)
+        s.update(2)
+        self.assertEqual(str(s), "[Square] (2) 3/4 - 5")
+        s.update(3, 10)
+        self.assertEqual(str(s), "[Square] (3) 3/4 - 10")
+        s.update(4, 10, 5)
+        self.assertEqual(str(s), "[Square] (4) 5/4 - 10")
+        s.update(5, 10, 5, 2)
+        self.assertEqual(str(s), "[Square] (5) 5/2 - 10")
 
     def test_to_dictionary(self):
-        s = Square(10, 2, 1, 50)
-        self.assertEqual(s.to_dictionary(), {'id': 50, 'size': 10, 'x': 2, 'y': 1})
-
-if __name__ == "__main__":
-    unittest.main()
+        s = Square(5, 3, 4, 1)
+        d = s.to_dictionary()
+        self.assertEqual(d, {'id': 1, 'size': 5, 'x': 3, 'y': 4})
