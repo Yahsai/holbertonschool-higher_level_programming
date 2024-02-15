c9a8d415defb96e33743e060c4aa84503036aff8
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 21:11:38 -0400

    Update test square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,31 +1,32 @@
 import unittest
 from models.square import Square
 
+
 class TestSquare(unittest.TestCase):
-    def test_init(self):
-        # Test the constructor
-        s = Square(5, 2, 3, 1)
-        self.assertEqual(s.id, 1)
-        self.assertEqual(s.size, 5)
-        self.assertEqual(s.x, 2)
-        self.assertEqual(s.y, 3)
-
     def test_str(self):
-        # Test the __str__ method
-        s = Square(5, 2, 3, 1)
-        self.assertEqual(str(s), "[Square] (1) 2/3 - 5")
+        s = Square(5, 3, 4, 1)
+        self.assertEqual(str(s), "[Square] (1) 3/4 - 5")
 
-    def test_size_getter_setter(self):
-        # Test the size getter and setter
-        s = Square(5, 2, 3, 1)
-        self.assertEqual(s.size, 5)
-
-        s.size = 10
+    def test_size(self):
+        s = Square(10)
         self.assertEqual(s.size, 10)
-        self.assertEqual(s.width, 10)
-        self.assertEqual(s.height, 10)
+        s.size = 20
+        self.assertEqual(s.size, 20)
+        self.assertEqual(s.width, 20)
+        self.assertEqual(s.height, 20)
 
-    # Add more tests as needed for other methods and edge cases
+    def test_update(self):
+        s = Square(5, 3, 4, 1)
+        s.update(2)
+        self.assertEqual(str(s), "[Square] (2) 3/4 - 5")
+        s.update(3, 10)
+        self.assertEqual(str(s), "[Square] (3) 3/4 - 10")
+        s.update(4, 10, 5)
+        self.assertEqual(str(s), "[Square] (4) 5/4 - 10")
+        s.update(5, 10, 5, 2)
+        self.assertEqual(str(s), "[Square] (5) 5/2 - 10")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_to_dictionary(self):
+        s = Square(5, 3, 4, 1)
+        d = s.to_dictionary()
+        self.assertEqual(d, {'id': 1, 'size': 5, 'x': 3, 'y': 4})
