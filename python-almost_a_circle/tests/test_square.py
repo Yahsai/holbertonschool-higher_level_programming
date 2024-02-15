7e430cce21bd495d7e8f4fad8e9a5330da883d4c
Yahsai <yahsaisantana1@gmail.com>
2024-02-14 23:24:21 -0400

    Update test square

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -30,3 +30,6 @@ class TestSquare(unittest.TestCase):
         s = Square(5, 3, 4, 1)
         d = s.to_dictionary()
         self.assertEqual(d, {'id': 1, 'size': 5, 'x': 3, 'y': 4})
+
+if __name__ == "__main__":
+    unittest.main()
