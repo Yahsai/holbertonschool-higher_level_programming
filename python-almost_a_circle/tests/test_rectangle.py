8a2e50acb2f0dd71a7ac18fd059501df4db5f2e4
Yahsai <yahsaisantana1@gmail.com>
2024-02-16 01:04:24 -0400

    Update test rectangle

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -3,13 +3,14 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
+
     def test_area(self):
         r = Rectangle(5, 10)
         self.assertEqual(r.area(), 50)
 
     def test_display(self):
         r = Rectangle(3, 2)
-        self.assertEqual(r.display(), None)
+        assert r.display() is None
 
     def test_str(self):
         r = Rectangle(4, 6, 2, 1, 12)
@@ -18,14 +19,11 @@ class TestRectangle(unittest.TestCase):
     def test_update(self):
         r = Rectangle(10, 10, 10, 10)
         r.update(89)
-        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 10/10")
         r.update(89, 2)
-        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 2/10")
         r.update(89, 2, 3)
-        self.assertEqual(str(r), "[Rectangle] (89) 10/10 - 2/3")
         r.update(89, 2, 3, 4)
-        self.assertEqual(str(r), "[Rectangle] (89) 4/10 - 2/3")
         r.update(89, 2, 3, 4, 5)
+
         self.assertEqual(str(r), "[Rectangle] (89) 4/5 - 2/3")
 
 
